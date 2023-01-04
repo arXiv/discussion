87745f6a32b1c050bc053c359376f979885ff5d1
Mark Nazzaro <marknazzaro2@gmail.com>
2023-01-04 15:12:14 -0500

    Slightly cleaner version, still has our custom media query detector, though

--- docs/fixcolortheme.js
+++ docs/fixcolortheme.js
@@ -6,16 +6,9 @@ darkModeMediaQuery.addEventListener('change', (e) => {
 function setColorTheme() {
   if (darkModeMediaQuery.matches) {
     document.body.setAttribute("data-md-color-scheme", "slate")
-    // I wanted to darken the Slate theme's background
-    // In theory, we should have been able to declaratively set --md-default-bg-color--dark
-    // But for some reason that's not a variable in the theme
-    // So we programmatically man-handle --md-default-bg-color on each change of color scheme
-    document.getElementsByTagName('body')[0].setAttribute("style", "--md-default-bg-color: #201f22")
-    document.getElementsByTagName('body')[0].setAttribute("style", "--md-default-fg-color: #d8d8d8")
   } 
   else {
     document.body.setAttribute("data-md-color-scheme", "default")
-    document.getElementsByTagName('body')[0].setAttribute("style", "--md-default-bg-color: var(--md-default-bg-color--light)")
   }
 }
 
